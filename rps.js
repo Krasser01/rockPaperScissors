8c4f12e738636f61fdc26b96572a1debae1a9852
Konstantin Krasser <konstantin.krasser@hotmail.com>
2023-01-30 22:39:00 +0100

    Not working anymore. Implementing step 5

--- rps.js
+++ rps.js
@@ -1,29 +1,25 @@
 const prompt=require("prompt-sync")({sigint:true});  /* FIX: ReferenceError: prompt is not defined */
-
-let computerScore = 0;
-let playerScore = 0;
-
 // Start 
-const items = ['rock', 'paper', 'scissors'];
-const getComputerChoice = items[Math.floor(Math.random() * items.length)]
-const getPlayerChoise = prompt('Enter: rock, paper or scissors ').trim().toLowerCase();
-const computerSelection = `${getComputerChoice}`;
-const playerSelection = `${getPlayerChoise}`;
-
-function game(computerSelection, playerSelection) {
-    if (playerSelection === computerSelection) { 
-            return 'Draw'
-    } else if (
-        (playerSelection=== 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            return 'Player Won'
-    } else {
-            return 'Computer Won'
+function game() {
+    const items = ['rock', 'paper', 'scissors'];
+    const getComputerChoice = items[Math.floor(Math.random() * items.length)]
+    const getPlayerChoise = 'paper' // prompt('Enter: rock, paper or scissors ').trim().toLowerCase();
+    const computerSelection = `${getComputerChoice}`;
+    const playerSelection = `${getPlayerChoise}`;
+    let computerScore = 0;
+    let playerScore = 0;
+    function playRound(playerSelection, computerSelection) {
+        for (let i  = 0; i < 5; i++) {
+            if (playerSelection === computerSelection) { 
+                    // Increase playerScore by one and repeat loop
+            } else if (
+                (playerSelection=== 'rock' && computerSelection === 'scissors') ||
+                (playerSelection === 'paper' && computerSelection === 'rock') ||
+                (playerSelection === 'scissors' && computerSelection === 'paper')) {
+                    playerScore += 1;
+            } else {
+                    computerScore += 1;
+            }
+        }
     }
-
 }
-
-console.log(computerSelection);
-console.log(playerSelection);
-console.log(game(computerSelection, playerSelection));
