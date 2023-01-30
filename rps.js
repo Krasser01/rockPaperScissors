572ce6d416e1987608d76f6793c6db97a8a0b464
Konstantin Krasser <konstantin.krasser@hotmail.com>
2023-01-30 23:20:02 +0100

    step three

--- rps.js
+++ rps.js
@@ -1,25 +1,42 @@
+/* ************************************************************************** */
+/*    Rock Paper Scissors                                                     */
+/*  > Minimalism is the way to go!                                            */
+/*    Koka 20230130                                                           */
+/* ************************************************************************** */
 const prompt=require("prompt-sync")({sigint:true});  /* FIX: ReferenceError: prompt is not defined */
-// Start 
-function game() {
-    const items = ['rock', 'paper', 'scissors'];
-    const getComputerChoice = items[Math.floor(Math.random() * items.length)]
-    const getPlayerChoise = 'paper' // prompt('Enter: rock, paper or scissors ').trim().toLowerCase();
-    const computerSelection = `${getComputerChoice}`;
-    const playerSelection = `${getPlayerChoise}`;
+
+//--------------------------------------Step ONE---------------------------------------------------------
+// Randomly return item of array
+const items = ['rock', 'paper', 'scissors'];
+const getComputerChoice = items[Math.floor(Math.random() * items.length)]
+
+//--------------------------------------Step TWO---------------------------------------------------------
+// Play one round
+const getPlayerChoise = 'paper' // prompt('Enter: rock, paper or scissors ').trim().toLowerCase();
+const computerSelection = `${getComputerChoice}`;
+const playerSelection = `${getPlayerChoise}`;
+
+    let playerScore = 0;    
     let computerScore = 0;
-    let playerScore = 0;
-    function playRound(playerSelection, computerSelection) {
-        for (let i  = 0; i < 5; i++) {
-            if (playerSelection === computerSelection) { 
-                    // Increase playerScore by one and repeat loop
-            } else if (
-                (playerSelection=== 'rock' && computerSelection === 'scissors') ||
-                (playerSelection === 'paper' && computerSelection === 'rock') ||
-                (playerSelection === 'scissors' && computerSelection === 'paper')) {
-                    playerScore += 1;
-            } else {
-                    computerScore += 1;
-            }
-        }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) { 
+            return 'DRAW'
+    } else if (
+        (playerSelection=== 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            return 'You won'
+    } else {
+            return 'Computer Won'
     }
 }
+
+//--------------------------------------STEP FINALE---------------------------------------------------------
+// Play five rounds
+
+function 
+
+
+
+console.log(playRound(playerSelection, computerSelection));
