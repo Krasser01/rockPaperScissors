994d34be84d38d09f00801b060dcc8243ce353b8
Konstantin Krasser <konstantin.krasser@hotmail.com>
2023-01-30 13:32:28 +0100

    restart of rps.js

--- rps_old.js
+++ rps_old.js
@@ -1,13 +1,22 @@
-// invoke the fuction `getComputerChoise`
-// A 'collection of items' = 'array'
-// In JavaScript an item of an Array is type of an Onject
-// Therefore we can use an array in order to invoke our function
-// Arrays store data in contiguous and adjacent memory location
-// Therefor we can call an element inside the array with the Math.floor(Math.random() *3 ) methode
-// Math.random generates a number between 0 and 1 that is neither 0 nor 1 
-// To get a numbe between 0 and 10 multiply * 10. Math.floor()*10
-// To get it to be a whole number apply Math.floor, which rounds down to the nearest whole nubmer.
-var getComputerChoise = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)]
-    console.log(getComputerChoise);
-// let computer pick random item from array
-// Return computer's chois of array
+// Invoke function that with variables for the game.
+const input = ['rock', 'paper', 'scissors']; 
+
+// Generate random variable for computerSelection
+let computerSelection = input[Math.floor(Math.random() * 3)];
+
+// let playerSelection = input[1]; Will select paper every time
+// !Edditlater Prompt user for input
+// Make input case sensitive by making everything lowercase.
+const prompt=require("prompt-sync")({sigint:true}); 
+let playerSelection = prompt('You do');
+
+//  Play a single round and declare a winner.
+
+
+
+
+
+
+
+console.log(computerSelection);
+console.log(playerSelection);
